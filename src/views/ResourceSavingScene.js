e820804227f73880441fbf7bc25099270e96dd6f
Krzysztof Magiera <krzys.magiera@gmail.com>
2018-09-18 12:07:59 +0200

    Use react-native-screens when available (#54)
    This change adds support for react-native-screens. Screens package makes it possible to use native primitives to render tabs container and its scenes such that they get properly mounted/unmounted when not visible.
    
    The support has only been added to `createBottomTabNavigator` as the material one renders its own container that's a part of a different package (react-native-tab-view).

--- src/views/ResourceSavingScene.js
+++ src/views/ResourceSavingScene.js
@@ -2,6 +2,7 @@
 
 import * as React from 'react';
 import { Platform, StyleSheet, View } from 'react-native';
+import { Screen, screensEnabled } from 'react-native-screens';
 
 type Props = {
   isVisible: boolean,
@@ -14,6 +15,7 @@ const FAR_FAR_AWAY = 3000; // this should be big enough to move the whole view o
 
 export default class ResourceSavingScene extends React.Component<Props> {
   render() {
+
     const {
       isVisible,
       optimizationsEnabled,
@@ -21,6 +23,11 @@ export default class ResourceSavingScene extends React.Component<Props> {
       style,
       ...rest
     } = this.props;
+
+    if (screensEnabled()) {
+      const { isVisible, ...rest } = this.props;
+      return <Screen active={isVisible ? 1 : 0} {...rest} />;
+    }
 
     return (
       <View
